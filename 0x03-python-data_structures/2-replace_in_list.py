ee94a1cac4a978e25cb81bae0d5ac8f035d61713
McConnell AlX <ik.ugwuanyi@gmail.com>
2022-12-28 13:39:43 -0800

    removed empty lines

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,7 @@
-#!/usr/bin/python3
-
-if __name__ == "__main__":
-    
-    def replace_in_list(my_list, idx, element):
-
-        if idx > len(my_list) - 1 or idx < 0:
-            return my_list
-        else:
-            my_list[idx] = element
-            return my_list
+#!/usr/bin/python3    
+def replace_in_list(my_list, idx, element):
+    if idx > len(my_list) - 1 or idx < 0:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
