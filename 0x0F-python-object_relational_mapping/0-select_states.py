824b971484619973b6e892c1cbc82f4391c578a1
McConnell AlX <ik.ugwuanyi@gmail.com>
2023-03-16 05:31:34 -0700

    added readme and 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM `states`")
+    [print(state) for state in cursor.fetchall()]
